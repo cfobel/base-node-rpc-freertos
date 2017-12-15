2eedb420f33d69f1c34719396a5815bbf9eb45c9
Christian Fobel <christian@fobel.net>
2017-12-15 16:05:46 -0500

    feat(freertos): include idle task handle
    Prior to this commit, a handle was not generated for the idle task.  This
    prevents, e.g., querying the stack high water mark for the idle task.
    
    This commit generates a handle for the idle task.

--- lib/FreeRTOS_ID507/src/FreeRTOSConfig.h
+++ lib/FreeRTOS_ID507/src/FreeRTOSConfig.h
@@ -93,8 +93,8 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil                 0
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetSchedulerState          0
-#define INCLUDE_xTaskGetIdleTaskHandle          0 // create an idle task handle.
-#define INCLUDE_xTaskGetCurrentTaskHandle       0
+#define INCLUDE_xTaskGetIdleTaskHandle          1 // create an idle task handle.
+#define INCLUDE_xTaskGetCurrentTaskHandle       1
 #define INCLUDE_uxTaskGetStackHighWaterMark     1
 
 #define configMAX(a,b)  ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
