c9ea7ef7dda7d3c12a84e7271dd4ad6d37901f50
Christian Fobel <christian@fobel.net>
2017-12-17 01:40:02 -0500

    chore(cleanup): remove deprecated code

--- src/base-node-rpc-freertos.ino
+++ src/base-node-rpc-freertos.ino
@@ -22,8 +22,7 @@ base_node_rpc_freertos::CommandProcessor<base_node_rpc_freertos::Node> command_p
 void TaskBlink( void *pvParameters );
 void TaskSerialListen( void *pvParameters );
 
-// Parse any new I2C data using `I2CHandler` for `Node` object.
-void i2c_receive_event(int byte_count) { node_obj.i2c_handler_.receiver()(byte_count); }
+void *maintask_handle;
 
 void setup() {
   node_obj.begin();
@@ -51,25 +50,7 @@ void setup() {
 }
 
 
-void loop() {
-  /*if (node_obj.serial_handler_.packet_ready()) {*/
-    /* A complete packet has successfully been parsed from data on the serial
-     * interface.
-     * Pass the complete packet to the command-processor to process the request.
-     * */
-    /*node_obj.serial_handler_.process_packet(command_processor);*/
-  /*}*/
-  /*if (node_obj.i2c_handler_.packet_ready()) {*/
-    /* A complete packet has successfully been parsed from data on the I2C
-     * interface.
-     * Pass the complete packet to the command-processor to process the request.
-     * */
-    /*node_obj.i2c_handler_.process_packet(command_processor);*/
-  /*}*/
-
-  /*// XXX Call user defined `loop` code.*/
-  /*node_obj.loop();*/
-}
+void loop() {}
 
 
 void TaskBlink(void *pvParameters)  // This is a task.
