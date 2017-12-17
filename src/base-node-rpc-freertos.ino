1b5a7e15769dff2385520ef86942f27a9780c563
Christian Fobel <christian@fobel.net>
2017-12-17 01:44:52 -0500

    feat(task/blink): shrink stack size to save RAM

--- src/base-node-rpc-freertos.ino
+++ src/base-node-rpc-freertos.ino
@@ -35,7 +35,7 @@ void setup() {
   xTaskCreate(
     TaskBlink
     ,  (const portCHAR *)"Blink"   // A name just for humans
-    ,  64  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  50  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  &task_blink_handle);
